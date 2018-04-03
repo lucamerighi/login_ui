747510b26bb0039a01050c2dde569a06a5cf8164
lucam <lucam@DESKTOP-C8OJ5N8>
2018-04-03 15:46:24 +0200

    Background

--- Login_UI/src/application/Login.java
+++ Login_UI/src/application/Login.java
@@ -4,6 +4,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Login {
@@ -12,12 +17,15 @@ public class Login {
 	public Login(Stage stage) {
 		// TODO Auto-generated constructor stub
 		BorderPane root = new BorderPane();
-		Button backBtn = new Button("Back to store");
+		Button backBtn = new Button("< Back to store");
 		Label notMember = new Label("Not a member?");
-		Button signUpBtn = new Button("Sign Up");
+		Button signUpBtn = new Button("Sign Up");		
 		
+		root.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, #263437, 	#648B8D)");
 		scene = new Scene(root, 800, 600);
-		
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
 		
 	}
 	
